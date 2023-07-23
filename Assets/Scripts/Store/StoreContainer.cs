8934364ae63f5ef890c0e323ef67e317b20cb600
Benjamin Zarate <benja_zarate@hotmail.es>
2023-07-22 23:12:08 -0400

    Update store container to not have items in cart

--- Assets/Scripts/Store/StoreContainer.cs
+++ Assets/Scripts/Store/StoreContainer.cs
@@ -9,12 +9,5 @@ namespace Store
     public class StoreContainer : ScriptableObject
     {
         public List<Item> itemsToSell;
-        public List<Item> itemsInCart;
-
-        public void AddItemToCart(Item newItem) 
-        {
-            itemsInCart.Add(newItem);
-            itemsToSell.Remove(newItem);
-        }
     }
 }
