43e5522eb63d88cd59644035c6e536bca57fc9dd
Benjamin Zarate <benja_zarate@hotmail.es>
2023-07-23 23:50:24 -0400

    Update player to play animations

--- Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -10,10 +10,12 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rigidbody2D;
     private Vector2 movementInput;
+    private Animator anim;
 
     private void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
@@ -24,5 +26,14 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
+        anim.SetBool("Walk", inputValue.Get<Vector2>().x != 0 || inputValue.Get<Vector2>().y != 0);
+        if (inputValue.Get<Vector2>().x < 0)
+        {
+            transform.localScale = new Vector2(-1, 1);
+        }
+        else if(inputValue.Get<Vector2>().x > 0) 
+        {
+            transform.localScale = new Vector2(1, 1);
+        }
     }
 }
