454d3af0aaa8d08efcd6e93bed7655bd7c682f8f
Benjamin Zarate <benja_zarate@hotmail.es>
2023-07-23 22:50:08 -0400

    Update item to have Icon

--- Assets/Scripts/Store/Item.cs
+++ Assets/Scripts/Store/Item.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 namespace Store.Items 
 {
-    public enum ItemType { HOOD = 0, WRIST = 1, TORSO = 2, SHOULDER = 3, ELBOW = 4, LEG = 5, BOOT = 6}
+    public enum ItemType { HOOD = 0, WRIST = 1, TORSO = 2, SHOULDER = 3, ELBOW = 4, LEG = 5, BOOT = 6, PELVIS = 7}
     [CreateAssetMenu(fileName = "Item", menuName = "Store/Item")]
     public class Item : ScriptableObject
     {
         public string ID;
         public string Name;
         public int Price;
-        public Sprite[] Icon;
+        public Sprite[] Clothes;
+        public Sprite Icon;
         public ItemType Type;
         public bool InInventory;
     }
